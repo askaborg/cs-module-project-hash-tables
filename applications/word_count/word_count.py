c0951522e70dbbf1ff945421e573dd8dc0155ae9
askaborg <hienle@askaborg.com>
2020-11-04 22:18:47 -0800

    apps part 1

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,7 +1,21 @@
 def word_count(s):
     # Your code here
+    edge_cases = [":", ";", ",", ".", "-", "+", "=", "/", "\\", "|", "[", "]", "{", "}", "(", ")", "*", "^", "&", "\""]
+    dictionary = dict()
+    lower_case_string = s.lower()
 
+    for item in edge_cases:
+        lower_case_string = lower_case_string.replace(item, "")
+    lower_case_string = lower_case_string.split()
 
+    for item in lower_case_string:
+        if item in dictionary:
+            x = dictionary[item]
+            x += 1
+            dictionary[item] = x
+        else:
+            dictionary[item] = 1
+    return dictionary
 
 if __name__ == "__main__":
     print(word_count(""))
